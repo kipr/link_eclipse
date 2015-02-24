6fd2757eecba1571324553bcbb2825f262d23df7
Braden McDorman <bmcdorman@gmail.com>
2015-02-23 21:15:17 -0600

    windows fixes

--- win.bat
+++ win.bat
@@ -1,7 +1,4 @@
 echo "Please wait..."
-putty -ssh root@%1 "rm -Rf /kovan/bin/%2 /kovan/archives/%2"
-putty -ssh root@%1 "cd /tmp && touch empty.kam && kissarchive -c surrogate 1.0.0 empty.kam 2> /dev/null"
-putty -ssh root@%1 "cd /tmp && mv *.kiss '/kovan/archives/%2' && mkdir -p '/kovan/bin/%2'"
-putty -ssh root@%1 "echo -e '#!/bin/bash\njava -cp /usr/lib/libkovan.jar:\$(dirname \$0) Main' > /kovan/bin/%2/%2 && chmod +x /kovan/bin/%2/%2"
+putty -ssh root@%1 -m win.cmd
 pscp -r "%3/bin/"* "root@%1:/kovan/bin/%2" 
 echo "done!"

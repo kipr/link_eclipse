7956aece53523a451d9047cd5b902e40eead8480
Nafis Zaman <Nafis.Zaman-1@ou.edu>
2015-02-23 21:24:51 -0600

    Windows fixes

--- win.bat
+++ win.bat
@@ -5,7 +5,7 @@ echo kissarchive -c surrogate 1.0.0 empty.kam ^2^> /dev/null >> win.cmd
 echo cd /tmp >> win.cmd
 echo mv ^*.kiss '/kovan/archives/%2' >> win.cmd
 echo mkdir -p '/kovan/bin/%2' >> win.cmd
-echo echo -e '#!/bin/bash\njava -cp /usr/lib/libkovan.jar:\$(dirname \$0) Main' ^> /kovan/bin/%2/%2 >> win.cmd
+echo echo -e '#!/bin/bash\njava -cp /usr/lib/libkovan.jar:$(dirname $0) Main' ^> /kovan/bin/%2/%2 >> win.cmd
 echo chmod +x /kovan/bin/%2/%2 >> win.cmd
 putty -ssh root@%1 -m win.cmd
 pscp -r "%3/bin/"* "root@%1:/kovan/bin/%2"

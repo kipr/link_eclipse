86d2ff875d620c28bc7c1b65acaebe703ae7d2f3
Jon Grasmeder <github@gras.org>
2015-03-11 19:27:34 -0400

    Create win_python.bat

---
+++ win_python.bat
@@ -0,0 +1,11 @@
+echo rm -Rf /kovan/bin/%2 /kovan/archives/%2 > win.cmd
+echo cd /tmp >> win.cmd
+echo touch empty.kam >> win.cmd
+echo kissarchive -c surrogate 1.0.0 empty.kam ^2^> /dev/null >> win.cmd
+echo cd /tmp >> win.cmd
+echo mv ^*.kiss '/kovan/archives/%2' >> win.cmd
+echo mkdir -p '/kovan/bin/%2' >> win.cmd
+echo echo -e '#!/bin/bash/npython $(dirname $0)/main.py' ^> /kovan/bin/%2/%2 >> win.cmd
+echo chmod +x /kovan/bin/%2/%2 >> win.cmd
+putty -ssh root@%1 -m win.cmd
+pscp -r "%3/"* "root@%1:/kovan/bin/%2"

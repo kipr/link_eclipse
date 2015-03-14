0e3e2c37b473fb5ce15cd97ffc5dad3a088b4f3c
Jon Grasmeder <github@gras.org>
2015-03-14 17:07:11 -0400

    Create osx_python.sh

---
+++ osx_python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ssh root@$1 "rm -Rf /kovan/bin/$2 /kovan/archives/$2"
+ssh root@$1 "cd /tmp && touch empty.kam && kissarchive -c surrogate 1.0.0 empty.kam 2> /dev/null"
+ssh root@$1 "cd /tmp && mv *.kiss '/kovan/archives/$2' && mkdir -p '/kovan/bin/$2'"
+ssh root@$1 "echo -e '#!/bin/bash\npython $(dirname \$0) Main' > /kovan/bin/$2/$2 && chmod +x /kovan/bin/$2/$2"
+scp -r "$3/src/*.py" "root@$1:/kovan/bin/$2" 
